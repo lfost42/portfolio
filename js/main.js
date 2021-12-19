3c1974d51799bc468a3685cf94ef57fe91886c60
Lynda Foster <lfost42@wgu.edu>
2021-12-19 09:54:58 -0800

    added carousel cards

--- js/main.js
+++ js/main.js
@@ -7,6 +7,21 @@
 		$nav = $('#nav'),
 		$wrapper = $('#wrapper');
 
+// Get cards
+var cards = $('.card-body');
+var maxHeight = 0;
+
+// Loop all cards and check height, if bigger than max then save it
+for (var i = 0; i < cards.length; i++) {
+  if (maxHeight < $(cards[i]).outerHeight()) {
+    maxHeight = $(cards[i]).outerHeight();
+  }
+}
+// Set ALL card bodies to this height
+for (var i = 0; i < cards.length; i++) {
+  $(cards[i]).height(maxHeight);
+}
+
 	// Breakpoints.
 		breakpoints({
 			xlarge:   [ '1281px',  '1680px' ],
